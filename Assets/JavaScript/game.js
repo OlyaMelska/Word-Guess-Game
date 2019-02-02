6cad093e68269e448345b3601b9efb35f80f2b06
candye_sweet <olyamelska@Olhas-MBP.home>
2019-02-01 21:04:30 -0500

    Basic working Game

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -6,30 +6,30 @@ let remainingGuesses = document.getElementById("remaining-guesses");
 let guessedLetters = document.getElementById("guessed-letters");
 let word;
 let letter;
-let number;
 let winsCount = 0;
 let totalGuesses = 12;
-let lettersArray = [];
 let pressedLetters = [];
+let string = [];
+let outputString = [];
 
 let data = [
   {
-    song: "West Coast",
+    song: "Mama",
     artist: "The Neighbourhood",
     imgURL: "../Images/theneighbourhood.jpg"
   },
   {
-    song: "Californication",
+    song: "Father",
     artist: "Red Hot Chili Peppers",
     imgURL: "../Images/redhotchilipeppers.jpg"
   },
   {
-    song: "Still D.R.E.",
+    song: "friend",
     artist: "Dr. Dre, Snoop Dogg",
     imgURL: "../Images/drdre.jpeg"
   },
   {
-    song: "All Eyez On Me",
+    song: "cat",
     artist: "2Pac",
     imgURL: "../Images/2pack.jpg"
   }
@@ -40,17 +40,20 @@ let data = [
 
 let generateRandom = value => Math.floor(Math.random() * value); // function that generates a random number - that would be an index of an object in our array "data"
 
-number = generateRandom(data.length);
-console.log(number);
-word = data[number].song;
-console.log(word);
-let string = [];
-for (let i = 0; i < word.length; i++) {
-  string[i] = "_ ";
-}
+function setNewWord() {
+  number = generateRandom(data.length);
+  word = data[number].song;
+  console.log(word);
+  string = [""];
 
-displayWord.textContent = string.join("");
-console.log(displayWord);
+  for (let i = 0; i < word.length; i++) {
+    string[i] = "_ ";
+  }
+  displayWord.textContent = string.join("");
+  guessedLetters.textContent = "";
+  console.log(displayWord);
+}
+setNewWord();
 
 function checkLetter(letter) {
   let letterIndex = word.toLowerCase().indexOf(letter);
@@ -61,9 +64,7 @@ function checkLetter(letter) {
       letterIndex = word.indexOf(letter, letterIndex + 1);
     }
     alreadyPressedLetters(letter);
-    console.log("displayword ", string.toString());
-    winTheGame(string.toString());
-    return true;
+    return string;
   } else {
     return false;
   }
@@ -74,27 +75,33 @@ function alreadyPressedLetters(letter) {
   guessedLetters.textContent = pressedLetters.join(" ");
   return pressedLetters;
 }
-remainingGuesses.textContent = totalGuesses;
 
 function winTheGame(value) {
-  if (value === word) {
+  value = value.toString().replace(/,/g, "");
+  if (value === word.toLowerCase()) {
     winsCount++;
     wins.textContent = winsCount;
-    console.log("you WON");
-    return true;
+    setNewWord();
+    totalGuesses = 12;
+    pressedLetters = [];
   } else {
-    console.log("you Lost");
     return false;
   }
 }
+remainingGuesses.textContent = totalGuesses;
 
 window.addEventListener("keydown", event => {
-  totalGuesses--;
-  key.textContent = event.key;
-  letter = event.key;
-
-  console.log(letter);
-  checkLetter(letter);
-
+  if (totalGuesses > 0) {
+    key.textContent = event.key;
+    letter = event.key;
+    totalGuesses--;
+    outputString = checkLetter(letter);
+    winTheGame(outputString);
+  } else {
+    alert("You LOST!!!");
+    setNewWord();
+    totalGuesses = 12;
+    guessedLetters.textContent = "";
+  }
   remainingGuesses.textContent = totalGuesses;
 });
